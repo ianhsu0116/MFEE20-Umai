0c48393f081f56fef41731edda8ba422f897e915
IanHsu <ianhsu0116@gmail.com>
2021-11-08 01:00:24 +0800

    1108AM - login 頁面完成90%(美觀需再加強)

---
+++ client/src/components/config/config.js
@@ -0,0 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
+export const FACEBOOK_CLIENT_ID = process.env.REACT_APP_FACEBOOK_CLIENT_ID;
