1570e072fcfd704a5a2761af55fdcf4d0a04cbbc
IanHsu <ianhsu0116@gmail.com>
2021-11-08 18:40:27 +0800

    1108PM - 新增memberSidebar的avatar 即時上傳功能

--- client/src/components/member/MemberSidebar.js
+++ client/src/components/member/MemberSidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { BsPersonCircle } from "react-icons/bs";
 import { AiOutlineUsergroupAdd } from "react-icons/ai";
@@ -11,19 +11,58 @@ import avatar from "../images/avatar.jpg";
 
 const MemberSidebar = (props) => {
   let { currentBoard, setCurrentBoard } = props;
+  const [currentAvatar, setCurrentAvatar] = useState(""); // 存avatar的二元編碼
+
+  // 切換sidebar內容
   const handleChangeBoard = (e) => {
     setCurrentBoard(e.target.innerText);
+  };
+
+  // 即時顯示上傳的avatar
+  const handleAvatarChange = (e) => {
+    let readFile = new FileReader(); //constructor 建構子(函數); 功能: 給初值
+    let file = e.target.files[0];
+    let imageType = /image.*/;
+
+    // 格式符合就顯示，否則提醒
+    if (file) {
+      if (file.type.match(imageType) && file.size < 4000000) {
+        // 將圖裝入，等待送到後端
+        //setAwsFile(file);
+
+        // 抓到二元編碼，即時顯示
+        readFile.readAsDataURL(file);
+        readFile.addEventListener("load", function () {
+          // 將二元編碼丟入state，即時顯示
+          setCurrentAvatar(readFile.result);
+        });
+      } else {
+        window.alert("只能上傳圖片歐！(檔案須小於4mb)");
+      }
+    }
   };
 
   return (
     <div className="MemberSidebar">
       <div className="MemberSidebar-container">
         <div className="MemberSidebar-container-avatar">
-          <img
-            src={avatar}
-            alt="使用者頭貼"
-            className="MemberSidebar-container-avatar-img"
+          <input
+            type="file"
+            id="avatar"
+            className="MemberSidebar-container-avatar-input"
+            onChange={handleAvatarChange}
+            multiple
           />
+          <label
+            htmlFor="avatar"
+            className="MemberSidebar-container-avatar-label"
+          >
+            <img
+              src={currentAvatar || avatar}
+              alt="使用者頭貼"
+              className="MemberSidebar-container-avatar-img"
+            />
+          </label>
           <FaPen className="MemberSidebar-container-avatar-pen" />
         </div>
         <div className="MemberSidebar-container-mamberName">Ian Hsu</div>
