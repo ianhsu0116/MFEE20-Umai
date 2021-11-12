ce07de0b0a0fb625104e8522bcb3377bde850b64
kiwi lin <cyc40405@gmail.com>
2021-11-12 18:51:31 +0800

    於討論區及故事牆新增了modal

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import MemberCenter from "./pages/MemberCenter/MemberCenter";
 import Login from "./components/member/Login";
+import Masonry from "./pages/Masonry/Masonry";
+import Forum from "./pages/Forum/Forum";
+import Discussion from "./pages/Forum/Discussion";
+import Try from "./pages/Try";
 
 // 測試元件區
 import DefaultStudentCard from "./components/DefaultStudentCard";
@@ -36,6 +40,18 @@ function App() {
         </Route>
         <Route path="/memberCenter" exact>
           <MemberCenter />
+        </Route>
+        <Route path="/masonry" exact>
+          <Masonry />
+        </Route>
+        <Route path="/Forum" exact>
+          <Forum />
+        </Route>
+        <Route path="/Discussion" exact>
+          <Discussion />
+        </Route>
+        <Route path="/Try" exact>
+          <Try />
         </Route>
       </Switch>
     </Router>
