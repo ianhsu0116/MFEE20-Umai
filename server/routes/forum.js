926b16aa89fabf42a03e419a0e4d1aadb71fd324
kiwi lin <cyc40405@gmail.com>
2021-11-27 00:17:39 +0800

    前端insert已設好

--- server/routes/forum.js
+++ server/routes/forum.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 const mysql = require("mysql");
 const Promise = require("bluebird");
-
+const bodyParser = require("body-parser");
 const connection = require("../utils/database");
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const upload = multer();
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
 
 router.get("/", async (req, res) => {
   try {
@@ -29,6 +34,20 @@ router.get("/:fourmId", async (req, res) => {
     console.log(error);
     res.json({ error: error });
   }
+});
+
+router.post("/insertArticle", upload.array(), async (req, res) => {
+  console.log("body", req.body);
+  // try {
+  //   let forumdatadetail = await connection.queryAsync(
+  //     "INSERT INTO forum_article(member_id,category_id,article_title,article_text,image_name,created_time,valid)VALUES(?,?,?,?,?,?,?),()"
+  //   );
+  //   console.log(forumdatadetail);
+  //   res.json({ forumdatadetail: forumdatadetail });
+  // } catch (error) {
+  //   console.log(error);
+  //   res.json({ error: error });
+  // }
 });
 
 module.exports = router;
